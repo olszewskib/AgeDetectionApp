571463480c5e9034f27f8e2aab870485c207ea52
Bartosz Olszewski <bartoszoloolszewski@gmail.com>
2023-12-29 14:23:53 +0100

    added file loading

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-import tkinter as tk
+from tkinter import *
+from tkdocviewer import *
 import ttkbootstrap as ttk
 import cv2 as cv
 from PIL import Image,ImageTk
 import face_recognition as fr
 import input_options_frame as iof
 from input_options import InputOptions
+from tkinter import filedialog
+
+def browse_files():
+    filepath = filedialog.askopenfilename(initialdir='/', title='Select a file')
+    return filepath
 
 # window
 window = ttk.Window(themename='journal')
@@ -17,26 +23,22 @@ window.bind('<Escape>', lambda e: window.quit())
 option_frame, selected_input = iof.get_options_frame(window)
 option_frame.pack()
 
+label_widget = ttk.Label(window)
+label_widget.pack()
+
+#v = DocViewer(window)
+#v.pack(side="top", expand=1, fill="both")
+#path = browse_files()
+#v.display_file(path)
+
+
 # camera
 vid = cv.VideoCapture(1)
 width, height = 800, 450
 vid.set(cv.CAP_PROP_FRAME_WIDTH,width)
 vid.set(cv.CAP_PROP_FRAME_HEIGHT,height)
 
-label_widget = ttk.Label(window)
-label_widget.pack()
-
-def open_camera():
-    
-    if(selected_input.get() != InputOptions.CAMERA.name):
-        label_widget.photo_image = None
-        return
-  
-    # Capture the video frame by frame 
-    _, frame = vid.read()
-    
-    frame = fr.detect_faces(frame)
-  
+def display_output(frame):
     # Convert image from one color space to other 
     opencv_image = cv.cvtColor(frame, cv.COLOR_BGR2RGBA) 
   
@@ -51,13 +53,44 @@ def open_camera():
   
     # Configure image in the label 
     label_widget.configure(image=photo_image) 
+    
+
+def open_camera():
+
+    if(selected_input.get() != InputOptions.CAMERA.name):
+        label_widget.photo_image = None
+        return
   
-    # Repeat the same process after every 10 mseconds 
+    _, frame = vid.read()
+    frame = fr.detect_faces(frame)
+    display_output(frame)
     label_widget.after(10, open_camera) 
   
+def read_photos():
+    
+    file_path = browse_files()
+    image = cv.imread(file_path)
+    if image is not None:
+        frame = fr.detect_faces(image)
+        display_output(frame)
+    else:
+        print("Nie można odczytać obrazu.")
+    
+    
+
+
+def run():
+    arg = selected_input.get()
+    
+    if(arg == InputOptions.CAMERA.name):
+        open_camera()
+    elif(arg == InputOptions.PHOTOS.name):
+        read_photos()
+    elif(arg == InputOptions.VIDEO.name):
+        print('video')
   
-# Create a button to open the camera in GUI app 
-run_button = ttk.Button(window, text="Run", command=open_camera) 
+
+run_button = ttk.Button(window, text="Run", command=run) 
 run_button.pack() 
 
 #run
