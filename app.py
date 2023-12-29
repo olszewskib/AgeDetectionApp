7d47981108f3668eb3e8a42da05df9913b5b1983
Bartosz Olszewski <bartoszoloolszewski@gmail.com>
2023-12-29 16:07:04 +0100

    added multiple files selection

--- app.py
+++ app.py
@@ -7,10 +7,23 @@ import face_recognition as fr
 import input_options_frame as iof
 from input_options import InputOptions
 from tkinter import filedialog
+import glob
+
+# Globals
+PADDING = 15
+processed_photos = []
 
 def browse_files():
     filepath = filedialog.askopenfilename(initialdir='/', title='Select a file')
     return filepath
+
+def browse_directories():
+    dirpath = filedialog.askdirectory(initialdir='/', title='Select directory')
+    return dirpath
+
+def on_resize(event):
+    if(selected_input.get() == InputOptions.CAMERA.name):
+        pass
 
 # window
 window = ttk.Window(themename='journal')
@@ -18,19 +31,19 @@ window.title('Age detection')
 window.geometry('1600x900')
 window.minsize(width='800', height='450')
 window.bind('<Escape>', lambda e: window.quit())
+window.bind('<Configure>', on_resize)
 
 # input options
 option_frame, selected_input = iof.get_options_frame(window)
-option_frame.pack()
+option_frame.pack(side='left', padx=PADDING, pady=PADDING, fill='both')
 
 label_widget = ttk.Label(window)
-label_widget.pack()
+label_widget.pack(side='right', padx=PADDING, pady=PADDING, expand=True)
 
 #v = DocViewer(window)
 #v.pack(side="top", expand=1, fill="both")
 #path = browse_files()
 #v.display_file(path)
-
 
 # camera
 vid = cv.VideoCapture(1)
@@ -53,7 +66,6 @@ def display_output(frame):
   
     # Configure image in the label 
     label_widget.configure(image=photo_image) 
-    
 
 def open_camera():
 
@@ -68,17 +80,29 @@ def open_camera():
   
 def read_photos():
     
-    file_path = browse_files()
+    processed_photos.clear()
+    files_listbox.delete(0, END)
+    dir_path = browse_directories()
+    file_paths = glob.glob(dir_path + '/*.jpg')
+
+    for file_path in file_paths:
+        processed_photo = process_photo(file_path)
+        processed_photos.insert(0,processed_photo)
+        file_name = file_path.split('/')[-1]
+        files_listbox.insert(0, file_name)
+    
+    return processed_photos
+
+def process_photo(file_path):
+    
     image = cv.imread(file_path)
     if image is not None:
         frame = fr.detect_faces(image)
         display_output(frame)
+        return frame
     else:
-        print("Nie można odczytać obrazu.")
+        print("Not a valid image")
     
-    
-
-
 def run():
     arg = selected_input.get()
     
@@ -90,8 +114,14 @@ def run():
         print('video')
   
 
-run_button = ttk.Button(window, text="Run", command=run) 
-run_button.pack() 
+run_button = ttk.Button(option_frame, text="Run", command=run) 
+run_button.pack(pady=PADDING) 
+
+files_title_label = ttk.Label(master = option_frame, text = 'Files', font = 'Calibri 24 bold').pack(anchor='w')
+files_listbox = Listbox(option_frame, height=200) # fix height with a constant value
+files_listbox.bind('<<ListboxSelect>>', lambda e: display_output(processed_photos[files_listbox.curselection()[0]]))
+    
+files_listbox.pack(pady=PADDING)
 
 #run
 window.mainloop()
